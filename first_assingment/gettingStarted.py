Fixed the Gini index calculation to match that of the lessons

first_assingment/gettingStarted.py
@@ -1,13 +1,15 @@
 import numpy as np
-from inequality.gini import Gini
+import math
+from collections import Counter
 
 credit_data = np.genfromtxt('credit.txt', delimiter=',', skip_header=True)
 
 
 def impurity(array):
-    gini = Gini(array)
-    return gini.g
-
+    cnt = Counter(array)
+    keys = list(cnt.keys())
+    gini = (cnt[keys[0]] * cnt[keys[1]]) / math.pow(len(array), 2)
+    return gini
 
 def bestsplit(x, y):
     n = len(x)

first_assingment/main.py
@@ -18,7 +18,9 @@ credit_data = np.genfromtxt('credit.txt', delimiter=',', skip_header=True)
 '''
 
 
-def tree_grow(x, y, nmin, minleaf, nfeat=len(x)):
+def tree_grow(x, y, nmin, minleaf, nfeat=None):
+    if nfeat is None:
+        nfeat = len(x)
     return ClassificationTreeDM(x, y, nmin, minleaf, nfeat)
 
 
@@ -39,7 +41,9 @@ def tree_grow(x, y, nmin, minleaf, nfeat=len(x)):
 '''
 
 
-def tree_grow_b(x, y, nmin, minleaf, nfeat=x.shape[0], m):
+def tree_grow_b(x, y, nmin, minleaf, nfeat=None, m=1):
+    if nfeat is None:
+        nfeat = x.shape[0]
     ret = []
     for i in range(m):
         indexes = np.random.choice(range(x.shape[0]), size=x.shape[0])