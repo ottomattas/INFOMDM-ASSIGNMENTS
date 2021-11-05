fbb851073f25ec167efcad79eca7cec8c740239e
ottomattas <otto.mattas@gmail.com>
2021-11-05 08:15:14 +0200

    Create new method for extracting important features

--- second_assignment/main.py
+++ second_assignment/main.py
@@ -13,6 +13,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB as NaiveBayes
 
 
+def important_features(vectorizer,classifier,n=20):
+    class_labels = classifier.classes_
+    feature_names = vectorizer.get_feature_names()
+    topn_class1 = sorted(zip(classifier.feature_count_[0], feature_names),reverse=True)[:n]
+    topn_class2 = sorted(zip(classifier.feature_count_[1], feature_names),reverse=True)[:n]
+    print("Important words in negative reviews")
+    for coef, feat in topn_class1:
+        print(class_labels[0], coef, feat)
+    print("-----------------------------------------")
+    print("Important words in positive reviews")
+    for coef, feat in topn_class2:
+        print(class_labels[1], coef, feat)
+
 def scoreBayes(train_ngram, train_classes, test_ngrams, test_classes, sfactor_start=0.1,
                         sfactor_end=10, sfactor_step=0.1):
 
@@ -107,16 +120,16 @@ if __name__ == '__main__':
     
     print("\nTraining Naïve-Bayes classifier with unigram features and obtaining hyperparameters...")
     bayes_unigram = scoreBayes(unigrams, trainclasses, testdocs, testclasses)
+    important_features(trainvectorizer,bayes_unigram["classifier"])
     print("Best Naïve-Bayes classifier for unigram features:\n" +
             "\tScore: " + str(bayes_unigram["score"]) +
-            "\tSmoothing factor: " + str(bayes_unigram["sfactor"]))
+            "\tSmoothing factor: " + str(bayes_unigram["sfactor"]))        
     print("\nTraining Naïve-Bayes classifier with unigram+bigram features and obtaining hyperparameters...")
     bayes_unibigram = scoreBayes(unibigrams, trainclasses, testdocs_bi, testclasses)
     print("Best Naïve-Bayes classifier for unigram+bigram features:\n" +
             "\tScore: " + str(bayes_unibigram["score"]) +
             "\tSmoothing factor: " + str(bayes_unibigram["sfactor"]))
-    
-    
+
     print("\nTraining Logistic Regression with unigram features and obtaining hyperparameters...")
     logreg_unigram = scoreLogReg(unigrams, trainclasses, testdocs, testclasses,
                           lambda_start=0.001, lambda_end=1, lambda_step=0.02)
@@ -147,13 +160,13 @@ if __name__ == '__main__':
 
     print("\nTraining Cost-Complexity Pruning Decision tree with unigram features and obtaining hyperparameters...")
     dtree_unigram = scoreDecisionTree(unigrams, trainclasses, testdocs, testclasses)
+    important_features(trainvectorizer,dtree_unigram["classifier"])
     print("Best Decision Tree classifier for unigram and bigram features:\n" +
             "\tScore: " + str(dtree_unigram["score"]) +
             "\tPruning alpha: " + str(dtree_unigram["alpha"]) +
             "\tMaximum depth of the tree: " + str(dtree_unigram["maxdepth"]) +
             "\tMinleaf: " + str(dtree_unigram["minleaf"]) +
             "\tMinsplit: " + str(dtree_unigram["minsplit"]))
-    
     print("\nTraining Cost-Complexity Pruning Decision tree with unigram+bigram features and obtaining hyperparameters...")
     dtree_unibigram = scoreDecisionTree(unigrams, trainclasses, testdocs, testclasses)
     print("Best Decision Tree classifier for unigram and bigram features:\n" +
